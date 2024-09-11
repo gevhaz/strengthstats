683e241bc03ac151b829faa2ca1339980eae79ae
Simon <gevhaz@tutanota.com>
2024-09-11 23:07:29 +0900

    Add homepage
    Added a homepage, for the root route handler. It's generated from a
    basic HTML template.

--- strengthstats/webapp/app.py
+++ strengthstats/webapp/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "<h1>Under construction</h1>"
+    """Render the homepage of the app."""
+    return render_template("index.html")
